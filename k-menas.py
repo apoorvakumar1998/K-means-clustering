e0c05274b53e11cdf13ecc0f77effccb454f8ea8
apoorvakumar1998 <31769042+apoorvakumar1998@users.noreply.github.com>
2019-03-04 14:02:04 +0530

    Update k-menas.py

--- k-menas.py
+++ k-menas.py
@@ -2,8 +2,7 @@ from copy import deepcopy
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-#plt.rcParams['figure.figsize'] = (16, 9)
-#plt.style.use('ggplot')
+
 
 # Importing the dataset
 data = pd.read_csv('tshirt.csv')
